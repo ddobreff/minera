ed35045108c9e25e3ec7403c53b49d53bc2c452b
skywills <william_kcool@hotmail.com>
2017-09-29 12:42:37 +0800

    add ewbfminer

--- application/models/ewbfminer_model.php
+++ application/models/ewbfminer_model.php
@@ -65,8 +65,25 @@ class Ewbfminer_model extends CI_Model {
 		
 		$ip = "127.0.0.1"; $port = 42000;
 
-        if ($network) list($ip, $port) = explode(":", $network);
-        
+		if ($network) list($ip, $port) = explode(":", $network);
+		
+		$host = 'http://'.$ip.':'.$port;
+		
+		//PULL IN EWBF DATA
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, $host);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $cmd);
+
+		$result = curl_exec($ch);
+		if (curl_errno($ch)) {
+			log_message("error",curl_error($ch));
+			return array("error" => true, "msg" => "Miner error");
+		}
+		curl_close ($ch);	
+		return json_decode($result,TRUE);		
+/*   		
         $host = 'http://'.$ip.':'.$port.'/'.$parse->method;
         $ctx = stream_context_create(array('http' => array('timeout' => 10)));
         $result = @file_get_contents($host, 0, $ctx);
@@ -74,73 +91,9 @@ class Ewbfminer_model extends CI_Model {
         if ($result !=null) {
             return json_decode($result);
         }
-        /*
-		$socket = $this->getsock($ip, $port);
-		if ($socket != null)
-		{
-			socket_write($socket, $cmd, strlen($cmd));
-			$line = $this->readsockline($socket);
-			socket_close($socket);
 
-			if (strlen($line) == 0)
-			{
-				$msg = "WARN: '$cmd' returned nothing\n";
-				return array("error" => true, "msg" => $msg);
-			}
-	
-			//print "$cmd returned '$line'\n";
-			if (substr($line,0,1) == '{') {
-				return json_decode($line);
-			}
-			
-			$data = array();
-			
-			$objs = explode('|', $line);
-			foreach ($objs as $obj)
-			{
-				if (strlen($obj) > 0)
-				{
-					$items = explode(',', $obj);
-					$item = $items[0];
-					$id = explode('=', $items[0], 2);
-					if (count($id) == 1 or !ctype_digit($id[1]))
-						$name = $id[0];
-					else
-						$name = $id[0].$id[1];
-			
-					if (strlen($name) == 0)
-						$name = 'null';
-			
-					if (isset($data[$name]))
-					{
-						$num = 1;
-						while (isset($data[$name.$num]))
-							$num++;
-						$name .= $num;
-					}
-			
-					$counter = 0;
-					foreach ($items as $item)
-					{
-						$id = explode('=', $item, 2);
-						if (count($id) == 2)
-							$data[$name][$id[0]] = $id[1];
-						else
-							$data[$name][$counter] = $id[0];
-			
-						$counter++;
-					}
-				}
-			}
-			
-			if (isset($data->STATUS->STATUS) && $data->STATUS->STATUS == 'E') {
-				return array("error" => true, "msg" => $data->STATUS->Msg);				
-			}
-			
-			return $data;
-		}
-		*/
 		return array("error" => true, "msg" => "Miner error");
+*/
     }
 
     function isOnline($network) {
